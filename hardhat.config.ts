b4cef2679963c29800a8ac116fdb2ade4803af16
billyjitsu <82428978+billyjitsu@users.noreply.github.com>
2023-08-18 23:23:42 -0700

    update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,64 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-foundry";
+require('dotenv').config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
-};
 
-export default config;
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.19",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: "0.8.13",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      }
+    ],
+  },
+  
+  networks: {
+    
+    hardhat: {
+      chainId: 1337
+    },
+  
+    // goerli: {
+    //   url: `${process.env.GOERLI_RPC_URL}`,
+    //   accounts: [`${process.env.PRIVATE_KEY}`],
+    //   gas: 300000000,
+    //   gasPrice: 100000000000,
+    // },
+    polygon: {
+      url: process.env.POLYGON_RPC_URL,
+      accounts: [`${process.env.PRIVATE_KEY}`],
+    },
+    mumbai: {
+      url: `${process.env.MUMBAI_RPC_URL}`,
+      accounts: [`${process.env.PRIVATE_KEY}`],
+      gas: 200000000,
+      gasPrice: 100000000000,
+    }
+  
+  },
+  etherscan: {
+  // Your API key for Etherscan
+  // Obtain one at https://etherscan.io/
+  apiKey: process.env.ETHERSCAN_API_KEY
+  }
+  };
+  
+  export default config;
+  
+  
